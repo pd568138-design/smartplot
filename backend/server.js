6ca463f2ae5c07232aceeb52a420dc1efa57e12b
Priya Dharshini <pd568138@gmail.com>
2026-05-31 18:59:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,31 @@
-
-const express=require("express");
-const cors=require("cors");
-const dotenv=require("dotenv");
-const connectDB=require("./config/db");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 dotenv.config();
 
-const app=express();
+const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Database Connection
 connectDB();
 
-app.use(require("./routes/plotRoutes"));
-app.use(require("./routes/authRoutes"));
-app.listen(5000,()=>{
-  console.log("Server Running On Port 5000");
+// Test Route
+app.get("/", (req, res) => {
+  res.send("SmartPlot API Running");
+});
+
+// Routes
+app.use("/api/plots", require("./routes/plotRoutes"));
+app.use("/api/auth", require("./routes/authRoutes"));
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Running On Port ${PORT}`);
 });
