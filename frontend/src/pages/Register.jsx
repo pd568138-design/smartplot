18e1526a35ccbe0db6a67db7d62604286012f165
Priya Dharshini <pd568138@gmail.com>
2026-05-31 19:12:11 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -18,7 +18,7 @@ export default function Register() {
     try {
 
       const response = await axios.post(
-        "http://localhost:5000/register",
+        "https://smartplot.onrender.com/register",
         data
       );
 
